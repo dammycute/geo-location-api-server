63add0ad4b85bc134a0cbde4e4c6f18e48562f14
Damilola Olawoore <Htcode@Oluwaseyis-MacBook-Pro.local>
2024-07-01 14:20:15 +0100

    merging the local with the upstream changes

--- hello_api/settings.py
+++ hello_api/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv( 'SECRET_KEY' )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['following-theodosia-htcode-942b7498.koyeb.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
