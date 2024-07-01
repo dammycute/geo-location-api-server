c2abb9ba147876b64e7e7dd25f18a4cafa193e31
Htcode <damilolaolawoore03@gmail.com>
2024-07-01 15:32:12 +0100

    merging the local with the upstream changes

--- hello_api/settings.py
+++ hello_api/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
